00b15a6d80d829597b063ac366207b8cd983b367
Yiqun Hu <yiqun.hu@gmail.com>
2016-03-30 20:53:00 +0800

    Refactor code

--- r2d2/spiders/news_36kr.py
+++ r2d2/spiders/news_36kr.py
@@ -15,9 +15,8 @@ class News36krSpider(scrapy.Spider):
         'http://www.36kr.com',
     )
 
-    def parse(self, response):
+    def parse_page(self, lasthour, response):
     	current = timezone("Singapore").localize(datetime.now())
-    	lasthour = current - timedelta(hours = 8)
     	earliest_time = current
     	
     	for x in response.xpath("//div[@class='articles J_articleList']//article"):
@@ -44,33 +43,18 @@ class News36krSpider(scrapy.Spider):
         	yield request
 
 
+    def parse(self, response):
+    	current = timezone("Singapore").localize(datetime.now())
+    	lasthour = current - timedelta(hours = 8)
+
+    	for request in self.parse_page(lasthour, response):
+    		yield request
+
     def parse_list(self, response):
     	lasthour = response.meta['lasthour']
-    	current = timezone("Singapore").localize(datetime.now())
-    	earliest_time = current
 
-    	for x in response.xpath("//div[@class='articles J_articleList']//article"):
-        	t = x.xpath("div//time[@class='timeago']/@title").extract()
-        	publish_time = None
-        	url = None
-        	if len(t)>0:
-        		publish_time = parser.parse(t[0])
-        	if publish_time is not None:
-        		if (publish_time<earliest_time):
-        			earliest_time = publish_time
-
-        		if (publish_time>lasthour):
-        			url = x.xpath("div//a[@class='title info_flow_news_title']/@href").extract()[0]
-        			if url is not None:
-        				request = scrapy.Request(urljoin(response.url, url), self.parse_news)
-            			request.meta['url'] = urljoin(response.url, url)
-            			yield request
-
-        if (earliest_time>lasthour):
-        	loadmore_url = response.xpath("//div[@class='articles J_articleList']//a[@class='load-more J_listLoadMore']/@href").extract()[0]
-        	request = scrapy.Request(urljoin(response.url, loadmore_url), self.parse_list)
-        	request.meta['lasthour'] = lasthour
-        	yield request	
+    	for request in self.parse_page(lasthour, response):
+    		yield request
 
 
     def parse_news(self, response):
