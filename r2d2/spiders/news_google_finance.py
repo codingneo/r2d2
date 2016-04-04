dbe0e12a6e1985fec2708db3435b90f733040296
Yiqun Hu <yiqun.hu@gmail.com>
2016-04-04 22:11:58 +0800

    Scrap google finance news

---
+++ r2d2/spiders/news_google_finance.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from collections import Counter
+from scrapy.contrib.loader import ItemLoader
+from googlefinance import getNews
+from r2d2.items import NewsItem
+
+
+class NewsGoogleFinanceSpider(scrapy.Spider):
+    name = "news_google_finance"
+    symbols = ['NASDAQ:PYPL', 'NYSE:SQ', 'NYSE:V', 'NYSE:MA']
+
+    def start_requests(self):
+    	for symbol in self.symbols:
+    		#stock = getQuotes(symbol)
+    		news = getNews(symbol)
+    		for article in news:
+    			if "hour" in article['d']:
+    				yield scrapy.Request(article['u'], self.parse)
+
+    def parse(self, response):
+    	print "parse: ", response.url
+    	title = response.xpath('//title/text()').extract()[0]
+    	p_elements = response.xpath("//body//p")
+    	parent_counts = Counter([' '.join(node.xpath("..//text()").extract()).strip() for node in p_elements])
+    	content = parent_counts.most_common()[0][0]
+    	# content = ' '.join(response.xpath("//body//p//text()").extract()).strip()
+    	item = NewsItem(url=response.url, title=title, content=content)
+    	yield item
+        
